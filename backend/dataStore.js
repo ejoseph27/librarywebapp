3a457d34be7d80db78983d5670ca7e056836fecf
Elvis Joseph <your_email@gmail.com>
2023-11-07 18:27:21 +0100

    backend

---
+++ backend/dataStore.js
@@ -0,0 +1,105 @@
+class StudentBookDatabase {
+    constructor() {
+        this.students = [
+            { name: 'Student1', postalCode: '1111' },
+            { name: 'Student2', postalCode: '2222' },
+            { name: 'Student3', postalCode: '3333' },
+            { name: 'Student4', postalCode: '4444' },
+            { name: 'Student5', postalCode: '5555' },
+        ];
+
+        this.books = [
+            { title: 'Book1', code: 'B001', available: true },
+            { title: 'Book2', code: 'B002', available: false },
+            { title: 'Book3', code: 'B003', available: true },
+            { title: 'Book4', code: 'B004', available: true },
+            { title: 'Book5', code: 'B005', available: true },
+        ];
+    }
+
+    // Method to validate a student's login
+    validateStudentLogin(name, postalCode) {
+        const student = this.students.find((s) => s.name === name && s.postalCode === postalCode);
+        return !!student; // Returns true if the student is found, false if not
+    }
+
+    // Method to add a new student
+    async addStudent(name, postalCode) {
+        return new Promise((resolve, reject) => {
+            // Check if the student already exists
+            if (this.students.find((s) => s.name === name && s.postalCode === postalCode)) {
+                // Student with the same name already exists, reject the Promise with an error message
+                reject(100);
+            } else {
+                // Add the student and resolve the Promise with a success message
+                this.students.push({ name, postalCode });
+                console.log("Updates", this.students);
+                resolve({
+                    messsage: "Student added successfully",
+                    data: this.students
+                });
+            }
+        });
+    }
+
+    async addBook(title, bookId) {
+        return new Promise((resolve, reject) => {
+            if (this.books.find((b) => b.title === title && b.code === bookId)) {
+                reject({ error: 'Book already in the database' });
+            }
+            else {
+                this.books.push({ title, bookId, avalilable: true });
+                console.log("Updated books databse", this.books);
+                resolve({ message: 'booked added sucessfully', data: this.books });
+            }
+        });
+    }
+
+    async borrowBook(title, available) {
+        return new Promise((resolve, reject) => {
+            if (this.books.find((b) => b.title === title && b.available === true)) {
+
+                const book = this.books.find((b) => b.title === title && b.available === true);
+                //console.log('borrowbook',book)
+                if (book) {
+                    const index = this.books.indexOf(book);
+                    console.log('Index', index);
+                    this.books[index].available = false;
+                    //console.log(this.books)
+                }
+                resolve({ message: 'you sucessfully borrowed the book', data: this.books })
+            }
+            else {
+                reject({ message: 'Book is not available' })
+            }
+        })
+    }
+
+    async returnBook(title, available) {
+        console.log('return book func');
+        return new Promise((resolve, reject) => {
+            if (this.books.find((b) => b.title === title && b.available === false)) {
+                const book = this.books.find((b) => b.title === title && b.available === false);
+
+                if (book) {
+                    const index = this.books.indexOf(book);
+                    //console.log('Index', index);
+                    this.books[index].available = true;
+                }
+                resolve({ message: 'you sucessfully returned the book', data: this.books })
+            }
+            else {
+                reject({ message: 'Book is not available' })
+            }
+        })
+    }
+
+    getBooks() {
+        return this.books;
+    }
+    getStudents() {
+        return this.students;
+    }
+}
+
+module.exports = StudentBookDatabase;
---
+++ backend/studentLogin.js
@@ -0,0 +1,101 @@
+const StudentBookDatabase = require('./dataStore');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors'); // Import the cors middleware
+app.use(bodyParser.json());
+// Enable CORS for all routes or specify specific routes
+app.use(cors());
+
+const studentDB = new StudentBookDatabase(); // Create an instance of the class
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+app.post('/api/login', (req, res) => {
+    // Retrieve the student's name and postal code from the request body
+    const { name, postalCode } = req.body;
+    const isLoginValid = studentDB.validateStudentLogin(name, postalCode);
+
+    // Perform validation (e.g., check if name and postal code match records)
+    if (isLoginValid) {
+        res.status(200).json({ message: 'Login success' });
+    } else {
+        res.status(401).json({ message: 'Login failed' });
+    }
+});
+
+app.post('/api/studentlist', async (req, res) => {
+
+    try {
+        //console.log("Request body", req.body);
+        // Assuming studentDB.addStudent returns a Promise
+        const studentData = await studentDB.addStudent(req.body.name, req.body.postalcode);
+        if (typeof studentData === 'object' && studentData !== null) {
+            // Success message
+            res.status(200).json({ message: 'Student added successfully', studentData });
+        } else {
+            // Error message
+            res.status(400).json({ error: 'name already exist', studentData });
+        }
+
+    } catch (error) {
+        // Handle any errors and send an error response with an appropriate status code
+        res.status(400).json({ error: 'Student already exist' });
+    }
+
+});
+
+app.post('/api/returnbook', async (req, res) => {
+    console.log('Request Body return book', req.body);
+    try {
+        const bookDb = studentDB.returnBook(req.body.title, req.body.code, req.body.available);
+        if (typeof bookDb === 'object' && bookDb !== null) {
+            // Success message
+            res.status(200).json({ message: 'book added successfully', bookDb });
+
+        }
+        else {
+
+            res.status(400).json({ error: 'unsucessfull' })
+
+        }
+
+    } catch (error) {
+        res.status(500).json('Internal Error');
+    }
+})
+
+app.post('/api/borrowbook', async (req, res) => {
+    console.log('Request Body borrow books', req.body);
+    try {
+        const bookDb = studentDB.borrowBook(req.body.title, req.body.bookId);
+        if (typeof bookDb === 'object' && bookDb !== null) {
+            // Success message
+            res.status(200).json({ message: 'book returned successfully', bookDb });
+        }
+        else {
+            res.status(400).json({ error: 'unsucessfull' })
+        }
+
+    } catch (error) {
+        res.status(500).json('Internal Error');
+    }
+})
+
+app.get('/api/booklist', (req, res) => {
+    console.log(req.body);
+    const bookData = studentDB.getBooks();
+    console.log(bookData);
+    res.json(bookData)
+});
+app.get('/api/studentlist', (req, res) => {
+    console.log(req.body);
+    const studentsData = studentDB.getStudents();
+    console.log(studentsData);
+    res.json(studentsData)
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
